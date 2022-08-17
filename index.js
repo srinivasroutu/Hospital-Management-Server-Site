e129247814a2975e795ef1072ab6fbf5f262406b
nferdous2 <noorjahanferdous029@gmail.com>
2022-08-17 22:23:37 +0600

    doctors add

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT | 5000
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tecyb.mongodb.net/?retryWrites=true&w=majority`;
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
-        console.log("Connected to MongoDB Successfully");
-    }finally{
-        // client.close();
+        const database = client.db("hospital");
+        constdoctorsCollection = database.collection("doctors");
+   
+        // get api for all doctor
+        app.get('/doctors', async (req, res) => {
+            const cursor =doctorsCollection.find({});
+            const doctors = await cursor.toArray();
+            res.send(doctors);
+        });
+        app.post('/doctors', async (req, res) => {
+            const doctor = req.body;
+            const resultP = awaitdoctorsCollection.insertOne(doctor);
+            console.log(resultP);
+            res.json(resultP)
+        });
+       
+    } finally {
+        // await client.close();
     }
 }
 
-run().catch(console.dir);
-
-app.use(cors());
-app.use(express.json())
-
 app.get('/', (req, res) => {
-  res.send('Hello Backend!')
+  res.send('Hello From Doctor!')
 })
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  console.log(`Doctors App listening  ${port}`)
 })
