70c19925c8ddd1c8ee2c5b42268e29249886b625
mahi1212 <mahi121.mr@gmail.com>
2022-09-15 16:45:14 +0600

    added doctors email filtering process

--- index.js
+++ index.js
@@ -1,42 +1,81 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { response } = require("express");
+var ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Middleware
 app.use(cors());
 app.use(express.json());
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run() {
-    try {
-        await client.connect();
-        const database = client.db("hospital");
-        constdoctorsCollection = database.collection("doctors");
-   
-        // get api for all doctor
-        app.get('/doctors', async (req, res) => {
-            const cursor =doctorsCollection.find({});
-            const doctors = await cursor.toArray();
-            res.send(doctors);
-        });
-        app.post('/doctors', async (req, res) => {
-            const doctor = req.body;
-            const resultP = awaitdoctorsCollection.insertOne(doctor);
-            console.log(resultP);
-            res.json(resultP)
-        });
-       
-    } finally {
-        // await client.close();
-    }
-}
 
-app.get('/', (req, res) => {
-  res.send('Hello From Doctor!')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tecyb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// console.log(uri)
+async function run() {
+  try {
+    await client.connect();
+    console.log("connected to db");
+
+    const database = client.db("SmartCare");
+    const doctorsCollection = database.collection("doctors");
+
+    app.get("/doctors", async (req, res) => {
+      const cursor = doctorsCollection.find({});
+      const doctors = await cursor.toArray();
+      res.json(doctors);
+    });
+
+    app.get("/doctors/:email", async (req, res) => {
+      const {email} = req.params;
+      const cursor = doctorsCollection.find({email});
+      const doctor = await cursor.toArray();
+      res.json(doctor);
+    });
+    // // User sending to db
+    // app.post("/users", async (req, res) => {
+    //   const user = req.body;
+    //   const result = await usersCollection.insertOne(user);
+    //   console.log(result);
+    //   res.json(result);
+    // });
+
+    // // User upsert function
+    // app.put("/users", async (req, res) => {
+    //   const user = req.body;
+    //   const filter = { email: user.email };
+    //   const options = { upsert: true };
+    //   const updateDoc = { $set: user };
+    //   const result = await usersCollection.updateOne(filter,updateDoc,options);
+    //   res.json(result);
+    // });
+
+    // // isStudent checking API
+    // app.get("/users/:email", async (req, res) => {
+    //   const email = req.params.email;
+    //   const query = { email: email };
+    //   const user = await usersCollection.findOne(query);
+    //   let isStudent = false;
+    //   if (user?.roles === "student") {
+    //     isStudent = true;
+    //   }
+    //   res.json({ student: isStudent });
+    // });
+
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Doctors App listening  ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
